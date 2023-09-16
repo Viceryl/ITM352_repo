77ee296f354d5c83ad831b4aeea7b066556bc523
viceryl <victoryl@hawaii.edu>
2023-09-15 15:38:26 -1000

    cope

---
+++ Lab5/lab5.js
@@ -0,0 +1,15 @@
+
+//declare variable per lab 5.1 instructions//
+let age=53;
+let fav_num=7;  //not my real fav number
+//let day_of_birth=8;
+let month_of_birth=11;
+
+// define calc
+let calc1 = age + fav_num / day_of_birth * month_of_birth;
+let calc2 = (age + fav_num) / day_of_birth * month_of_birth;
+
+//Output calc 1  & 2 to DOM
+document.getElementById("result1").innerHTML = calc1;
+document.getElementById("result2").innerHTML = calc2;
+
