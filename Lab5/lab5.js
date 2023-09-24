34087247f326b5937febe1ebb80b89a7eb2c4fba
viceryl <victoryl@hawaii.edu>
2023-09-24 13:47:23 -1000

    Lab 5 part 1

--- Lab5/lab5.js
+++ Lab5/lab5.js
@@ -1,15 +1,16 @@
 
 //declare variable per lab 5.1 instructions//
-let age=53;
-let fav_num=7;  //not my real fav number
-//let day_of_birth=8;
-let month_of_birth=11;
+let age=20;  // Not my real age
+let fav_num=4;  //not my real fav number
+let day_of_birth=10;
+let month_of_birth=11; //not my real birthmonth
 
 // define calc
-let calc1 = age + fav_num / day_of_birth * month_of_birth;
-let calc2 = (age + fav_num) / day_of_birth * month_of_birth;
+let calc1 = age+fav_num/day_of_birth*month_of_birth;
+let calc2 = (age+fav_num)/day_of_birth*month_of_birth;
 
 //Output calc 1  & 2 to DOM
 document.getElementById("result1").innerHTML = calc1;
 document.getElementById("result2").innerHTML = calc2;
 
+ 
